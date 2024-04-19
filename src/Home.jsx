2fe6878edd2768d98fc7ed70af6f173e20f93f6a
MiguelHort <miguel.d.hort@gmail.com>
2024-04-19 16:38:36 -0300

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,81 @@
+import react from 'react'
+
+import './style/home.scss';
+
+
+function Home() {
+  return (
+    <div>
+      <header>
+        <a href="">DRIVETRIBES</a>
+        <a href="">JEREMY CLARKSON</a>
+        <a href="">JAMES MAY</a>
+        <a href="">RICHARD HAMMOND</a>
+        <a href="">VIDEOS & REVIEWS</a>
+        <a href="">NEWS & CAR CULTURE</a>
+        <a href="">DRIVETRIBES</a>
+      </header>
+      <main>
+        <section>
+          <div>
+            <div>
+              <h1>THE SOCIAL NETWORK FOR PETROLHEADS</h1>
+              <input type="text" />
+              <button>JOIN DRIVETRIBE</button>
+              <a href=""><img src="" alt="" />OR SING UP WITH FACEBOOK</a>
+            </div>
+            <div>
+              <div>
+                <p>M</p>
+                <p>MERCEDES</p>
+              </div>
+              <div>
+                <p>M</p>
+                <p>MERCEDES</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p>M</p>
+                <p>MERCEDES</p>
+              </div><div>
+                <p>M</p>
+                <p>MERCEDES</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h1>JOIN OR CREATE YOUR TRIBES</h1>
+            <div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div><div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div><div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div><div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div><div>
+              <p>M</p>
+              <p>MERCEDES</p>
+            </div>
+            <button></button>
+          </div>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
